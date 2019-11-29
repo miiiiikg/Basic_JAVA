84ba2d8e9815afa9e2cf325b0d4343c42dd8fc0f
miiiiikg <58206099+miiiiikg@users.noreply.github.com>
2019-11-29 16:53:13 +0900

    전역변수, 지역변수
    메서드 오버로딩 vs 메서드 오버라이딩

---
+++ problem/ReversePrint.java
@@ -0,0 +1,43 @@
+package problem;
+
+import java.util.Scanner;
+
+public class ReversePrint {
+	// 조건
+	// 1. 사용자가 임의의 정수를 입력
+	// 2. 사용자가 입력한 정수를 5까지 출력
+	// 출력 예제
+	// 입력 >> 5
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	
+		/*
+		 * public static void main(String[] args) {
+		 *
+		 *
+		 *
+		 *
+		 *
+			
+			System.out.println("숫자를 입력하세요:");
+			Scanner sc = new Scanner(System.in);
+			
+			int num = sc.nextInt(); 
+			for(; num >=5) {
+				
+			}System.out.println(sum);
+			
+			
+			
+		}
+		*/
+		public static void main (String[] args) {
+			int end = 0;
+			for(int num = 1;  )
+		}
+	
+	
+}
